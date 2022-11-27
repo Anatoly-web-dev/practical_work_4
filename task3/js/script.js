91160f382a581edbc47df643f4bf195b8ff2db70
Anatoly Kostrykin <gupekmop777@mail.ru>
2022-11-27 23:02:45 +0300

    Сompleted task 'improved traffic light'

--- task3/js/script.js
+++ task3/js/script.js
@@ -24,12 +24,49 @@ trafficLightEl.addEventListener('click', makeGreen);
 
 // ---------------------------- Задача 2 -----------------------------------
 
-/* смена цвета происходит при клике на любой из них. 
-Каждый круг может принимать только один из цветов.
-При этом, когда один из кругов становится «цветным», остальные становятся черными. 
-Порядок смены цвета тот же, что и в изначальном задании. 
+/* 
+Смена цвета происходит при клике на любой из них.          									+++
+Каждый круг может принимать только один из цветов.												+++ 
+При этом, когда один из кругов становится «цветным», остальные становятся черными.  +++
+Порядок смены цвета тот же, что и в изначальном задании.       							+++
 */
+
+// получаем коллекцию нужных нам элементов
 const trafficLightElements = document.querySelectorAll('.task_2__item');
+// присваиваем каждому элементу коллекции константу (Деструктурирующее присваивание)
+const [greenCircle, yellowCircle, redCircle] = trafficLightElements;
+
+// создаем функции меняющие цвета у элементов на нужные
+function makeColorGreen() {
+	greenCircle.style.background = 'green';
+	yellowCircle.style.background = 'black';
+	redCircle.style.background = 'black';
+}
+
+function makeColorYellow() {
+	greenCircle.style.background = 'black';
+	yellowCircle.style.background = 'yellow';
+	redCircle.style.background = 'black';
+}
+
+function makeColorRed() {
+	greenCircle.style.background = 'black';
+	yellowCircle.style.background = 'black';
+	redCircle.style.background = 'red';
+}
+
+// перебираем коллекцию методом forEach,
+trafficLightElements.forEach(element =>
+	element.addEventListener('click', // каждому элементу коллекции назначаем событие
+		() => {
+			// функции смены цветов у элементов выполняются с небольшой задержкой для плавности
+			setTimeout(makeColorGreen, 300);
+			setTimeout(makeColorYellow, 1000);
+			setTimeout(makeColorRed, 1500);
+		},
+		// { once: true } // - обработчик будет автоматически удалён после выполнения 
+		// (расскомментировать код выше если нужно, чтобы событие для каждого элемента произошло всего 1 раз)
+	));
 
 
 
@@ -46,21 +83,12 @@ const trafficLightElements = document.querySelectorAll('.task_2__item');
 
 
 
-
-
-
-
-
-
-
-
-
-//отменяет при клике переход у ссылок в футере
+/*отменяет при клике переход у ссылок в футере	*/
 const linksStop = document.querySelectorAll('.footer-links__link');
 linksStop.forEach(link => {
 	link.addEventListener('click',
 		(event) => {
 			event.preventDefault();
 			link.classList.add('link-stop');
-		})
+		});
 });
